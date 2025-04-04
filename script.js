a966f2cb453b7c9993e11fc08acc535517ce0c10
InnovascriptBO <innovascript.bo@innovaschools.edu.pe>
2025-04-03 20:37:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -569,3 +569,15 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  document.getElementById('installBtn').style.display = 'block';
+});
+
+document.getElementById('installBtn').addEventListener('click', () => {
+  deferredPrompt.prompt();
+});
+
